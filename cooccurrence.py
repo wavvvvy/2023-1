30b6e301664d74c8cade37dad2a1822f6e208d4c
jjy <129416211+wavvvvy@users.noreply.github.com>
2023-05-26 22:39:32 +0900

    main

---
+++ cooccurrence.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+from dict_to_mat import dict_to_mat
+
+def cooccurrence(tokens, vocab_to_idx, window=2, min_cooccurrence=2):
+    counter = defaultdict(int)
+    for s, tokens_i in enumerate(tokens):
+        vocabs = [vocab_to_idx[w] for w in tokens_i if w in vocab_to_idx]
+        n = len(vocabs)
+        for i, v in enumerate(vocabs):
+            if window <= 0:
+                b, e = 0, n
+            else:
+                b = max(0, i - window)
+                e = min(i + window, n)
+            for j in range(b, e):
+                if i == j:
+                    continue
+                counter[(v, vocabs[j])] += 1
+                counter[(vocabs[j], v)] += 1
+    counter = {k:v for k,v in counter.items() if v >= min_cooccurrence}
+    n_vocabs = len(vocab_to_idx)
+    return dict_to_mat(counter, n_vocabs, n_vocabs)
